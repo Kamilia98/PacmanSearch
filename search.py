3cec953b9838bf79ab287622a831b5b4737340ad
Kamilia98 <kamiliaahmed01@gmail.com>
2023-10-27 22:49:45 +0200

    CommentsAdded

--- search.py
+++ search.py
@@ -79,25 +79,39 @@ def depthFirstSearch(problem: SearchProblem):
     print("Start's successors:", problem.getSuccessors(problem.getStartState()))
     """
     "*** YOUR CODE HERE ***"
-
+    # Get the initial state from the problem
     start = problem.getStartState()
+    print(start)
+
+    # Check if the initial state is already the goal state
     if problem.isGoalState(start):
         return []
 
+    # Initialize a stack for DFS and a list to keep track of visited nodes
     myQueue = util.Stack()
     visited = []
-    # setting my first path, its actions and cost
+
+    # Push the initial node and an empty list of actions onto the stack
     myQueue.push((start, []))
 
+    # Start the DFS loop
     while myQueue:
+        # Pop the current node and its associated actions from the stack
         currentNode, actions = myQueue.pop()
+
+        # Check if the current node has not been visited before
         if currentNode not in visited:
-            visited.append(currentNode)
+            visited.append(currentNode)  # Mark the node as visited
+            # Check if the current node is the goal state
 
             if problem.isGoalState(currentNode):
-                return actions
+                return actions  # Return the list of actions to reach the goal state
+
             for nextNode, action, cost in problem.getSuccessors(currentNode):
+                # Create a new list of actions by appending the current action.
                 newAction = actions + [action]
+
+                # Push the next node and the new list of actions onto the stack for further exploration.
                 myQueue.push((nextNode, newAction))
 
     util.raiseNotDefined()
@@ -106,26 +120,42 @@ def depthFirstSearch(problem: SearchProblem):
 def breadthFirstSearch(problem: SearchProblem):
     """Search the shallowest nodes in the search tree first."""
     "*** YOUR CODE HERE ***"
+    # Get the starting state of the problem.
     start = problem.getStartState()
+
+    # Check if the starting state is already a goal state.
     if problem.isGoalState(start):
-        return []
+        return []  # If it's a goal state, no need to search, return an empty list
+
+    # Initialize a queue for BFS and a list to keep track of visited nodes
     myQueue = util.Queue()
     visited = []
-    # setting my first path, its actions and cost
-    myQueue.push((start, [], 0))
 
+    # Initialize the queue with the starting state and and empty list of actions
+    myQueue.push((start, []))
+
+    # Start the BFS loop.
     while myQueue:
-        currentNode, actions, currentCost = myQueue.pop()
+        # Get the current node and its associated actions from the queue
+        currentNode, actions = myQueue.pop()
+
+        # Check if the current node has not been visited before
         if currentNode not in visited:
-            visited.append(currentNode)
+            visited.append(currentNode)  # Mark the current node as visite
 
-            if problem.isGoalState(currentNode):
-                return actions
+        # Check if the current node is a goal state.
+        if problem.isGoalState(currentNode):
+            return actions  # If it's a goal state, return the list of actions
 
-            for nextNode, action, cost in problem.getSuccessors(currentNode):
-                newAction = actions + [action]
-                newCost = currentCost + cost
-                myQueue.push((nextNode, newAction, newCost))
+        # Explore the successors of the current node.
+        for nextNode, action, cost in problem.getSuccessors(currentNode):
+            newAction = actions + [
+                action
+            ]  # Extend the list of actions with the new action
+
+            myQueue.push(
+                (nextNode, newAction)
+            )  # Add the successor to the queue for further exploration
 
     util.raiseNotDefined()
 
@@ -133,28 +163,50 @@ def breadthFirstSearch(problem: SearchProblem):
 def uniformCostSearch(problem: SearchProblem):
     """Search the node of least total cost first."""
     "*** YOUR CODE HERE ***"
+    # Get the initial state of the problem
     start = problem.getStartState()
-    if problem.isGoalState(start):
-        return []
 
+    # Check if the initial state is the goal state
+    if problem.isGoalState(start):
+        return []  # If it is, return an empty list, as we're already at the goal.
+
+    # Create a list to keep track of visited nodes
     visited = []
 
-    pQueue = util.PriorityQueue()
-    # ((coordinate/node , action to current node , cost to current node),priority)
-    pQueue.push((start, [], 0), 0)
+    # Create a priority queue for uniform cost search
+    myQueue = util.PriorityQueue()
 
-    while not pQueue.isEmpty():
-        currentNode, actions, currentCost = pQueue.pop()
+    # Add the start node with its associated action and cost to the priority queue with a priority of 0
+    myQueue.push((start, [], 0), 0)
+
+    # Main loop for uniform cost search
+    while not myQueue.isEmpty():
+        # Get the node with the lowest cost from the priority queue
+        currentNode, actions, currentCost = myQueue.pop()
+
+        # Check if the current node has been visited before
         if currentNode not in visited:
-            visited.append(currentNode)
+            visited.append(currentNode)  # Mark the current node as visited
 
+            # Check if the current node is the goal state
             if problem.isGoalState(currentNode):
-                return actions
+                return actions  # If it is, return the list of actions taken to reach the goal
 
+            # Explore the successors of the current node
             for nextNode, action, cost in problem.getSuccessors(currentNode):
+                # Create a new list of actions by adding the current action
                 newAction = actions + [action]
-                priority = currentCost + cost
-                pQueue.push((nextNode, newAction, priority), priority)
+
+                # Calculate the new cost by adding the current cost
+                newCost = currentCost + cost
+
+                # The priority is set to the new cost for uniform cost search
+                priority = newCost
+
+                # Add the next node with its new action and cost to the priority queue
+                myQueue.push((nextNode, newAction, newCost), priority)
+                print(myQueue.priority)
+
     util.raiseNotDefined()
 
 
@@ -169,30 +221,44 @@ def nullHeuristic(state, problem=None):
 def aStarSearch(problem: SearchProblem, heuristic=nullHeuristic):
     """Search the node that has the lowest combined cost and heuristic first."""
     "*** YOUR CODE HERE ***"
+    # Get the initial state of the problem
     start = problem.getStartState()
+
+    # Check if the initial state is the goal state
     if problem.isGoalState(start):
         return []
 
-    visitedNodes = []
-
+    # Initialize a priority queue for the open set and a list to track visited nodes
     myQueue = util.PriorityQueue()
-    # ((coordinate/node , action to current node , cost to current node),priority)
+    visited = []
+
+    # Push the initial state, empty actions list, and zero cost to the open set with priority 0
     myQueue.push((start, [], 0), 0)
 
+    # Main A* search loop.
     while myQueue:
+        # Pop the node with the lowest priority (lowest estimated total cost)
         currentNode, actions, currentCost = myQueue.pop()
 
-        if currentNode not in visitedNodes:
-            visitedNodes.append(currentNode)
+        # Check if the current node has already been visited to avoid revisiting it
+        if currentNode not in visited:
+            visited.append(currentNode)
 
-            if problem.isGoalState(currentNode):
-                return actions
+        # Check if the current node is the goal state
+        if problem.isGoalState(currentNode):
+            return actions
 
-            for nextNode, action, cost in problem.getSuccessors(currentNode):
-                newAction = actions + [action]
-                newCostToNode = currentCost + cost
-                heuristicCost = newCostToNode + heuristic(nextNode, problem)
-                myQueue.push((nextNode, newAction, newCostToNode), heuristicCost)
+        # Explore the successors of the current node
+        for nextNode, action, cost in problem.getSuccessors(currentNode):
+            # Create new action list and update the cost
+            newAction = actions + [action]
+            newCost = currentCost + cost
+
+            # Calculate the estimated total cost using the heuristic function
+            heuristicCost = newCost + heuristic(nextNode, problem)
+
+            # Push the next node onto the priority queue with its heuristic cost
+            myQueue.push((nextNode, newAction, newCost), heuristicCost)
 
     util.raiseNotDefined()
 
